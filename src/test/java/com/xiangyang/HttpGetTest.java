5a6c39bf417a0ae552b7c5399ad19d49944d6d80
xiangyangaa <15138778469@163.com>
2021-01-25 16:40:43 +0800

    httpClient4 修改

--- src/test/java/com/xiangyang/HttpGetTest.java
+++ src/test/java/com/xiangyang/HttpGetTest.java
@@ -3,9 +3,15 @@ package com.xiangyang;
 import com.xiangyang.http.client.model.HttpResponseResult;
 import com.xiangyang.http.client.util.HttpToolsClient;
 
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpGetTest {
     public static void main(String[] args) {
-        HttpResponseResult<String> str=HttpToolsClient.getStrByGetUrl("http://httpbin.org/get");
+        Map<String,Object> map = new HashMap<>();
+        map.put("aa","中文");
+        HttpResponseResult<String> str=HttpToolsClient.getStrByGetParameters("http://httpbin.org/get",map, "ISO-8859-1");
         System.out.println(str);
     }
 }
